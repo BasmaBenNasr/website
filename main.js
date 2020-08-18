e84aabce92ed1ef4d1528761a3defc8c895abf0a
BenNasrBasma <bennasrbasma97@gmail.com>
2020-08-18 23:10:44 +0200

    trying to found a sources to link to my webpage

--- main.js
+++ main.js
@@ -1,11 +1,53 @@
+$(document).ready(function(){
+	var flag = 0;
+	$('.ul_2 .CLOTHING').css('background-color','green');
+   $('.ul_2 li').click(function(){
+   	//this to avoid repetition and it refers to the element before it or value
+    	$(this).css('background-color','green');
 
-//here i created the name of the website and style it
-//$('h1').text('RanBOU').css('color','white').css('font-size','40px').css('margin-top','-1000px').css('font-family','Pacificos')
+    	//The siblings() method returns all sibling elements of the selected element/ Sibling elements are elements that share the same parent.(w3s)
 
-//i want the search to be clickable
+    	$(this).siblings().css('background-color','#2c3e50');
+    });
+});
 
-//$("button").click(function(){
-  //  $("").attr("href", "");
- // });
+//this for the images of clothing
+$('.CLOTHING').click(function(){
+	flag = 0;
+	$(.'images_clothes').css('display','block');
+
+	$('.photo').animate({height: '200px' , width: '240px'});
+$(.'photo').siblings().css('display','block');
+
+//i seted The class img to display none to be completely removed
+$('.images_clothes').siblings().('display','none');
+
+});
+
+//this for img accessories
+$('.bags').click(function(){
+	flag = 0;
+	$(.'images_bags').css('display','block');
+
+	$('.photo').animate({height: '200px' , width: '240px'});
+$(.'photo').siblings().css('display','block');
+$('.images_bags').siblings().('display','none');
+
+});
+
+//on make up too
+$('.MAKE-UP').click(function(){
+	flag = 0;
+	//	Displays an element as a block element /It starts on a new line, and takes up the whole width
+	$(.'images_makeup').css('display','block');
+
+	$('.photo').animate({height: '200px' , width: '240px'});
+$(.'photo').siblings().css('display','block');
+$('.images_makeup').siblings().('display','none');
+
+});
+//this part from w3school i want when i click on the photo it will have animation it will be bigger and then small again
+//but i think it didnt work 
+//i need to analyze it again
 
 
