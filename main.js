91b1dcf9bd07bdfec67c5b12e0b84a11d61fc119
BenNasrBasma <bennasrbasma97@gmail.com>
2020-08-18 03:35:36 +0200

    trying to find how to click on the text

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 
 
 //$('img').attr('src',"https://images.pexels.com/photos/4171757/pexels-photo-4171757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940").css('width','80%')
-$('h1').text('RanBOU').css('color','darkcyan').css('font-size','40px').css('text-align','center')
+
+//here i created the name of the website and style it
+$('h1').text('RanBOU').css('color','darkcyan').css('font-size','40px')
+//i want the search to be clickable
+$('.click').css('text-align','right').css('color','red').css("font-size","20px")
